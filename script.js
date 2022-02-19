43577b4477fcb70b319d322b28e8f9e7e5d9efee
Raphael Rosa <raphaelrodrigues93@gmail.com>
2022-02-19 18:36:44 -0300

    configuração do calendario no javascript

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 //quando usamos data.getMonth() retorna uma lista com Jan de indice 0
 const data = new Date();
 
+const renderizarCalendario = () => {
+    data.setDate(1)
 
+//Criando variavel para gerar lista numerica dos dias
+const diasDoMes = document.querySelector('.dias');
+let dias = "";
+//criando variavel para identificar o ultimo dia de cada mes
+const ultimoDia = new Date(data.getFullYear(), data.getMonth() + 1,0).getDate();
+//criando variavel para identificar o primeiro dia de cada mes
+const diasMesAnterior = new Date(data.getFullYear(), data.getMonth(),0).getDate();
+
+const primeiroDiaIndex = data.getDay()
+
+const ultimoDiaIndex = new Date(data.getFullYear(), data.getMonth() + 1,0).getDay();
+
+const proximosDias = 7 - ultimoDiaIndex - 1;
 //Lista de meses
 const meses = [
     "Jan",
@@ -23,19 +38,37 @@ const meses = [
 document.querySelector('.mes-titulo').innerHTML = meses[data.getMonth()];
 
 //DESCOBRIR COMO DEIXAR A DATA EM PORTUGUES ESCRITA DA FORMA QUE ESTÁ
-document.querySelector('.mes-data-completa').innerHTML = data.toDateString();
+document.querySelector('.mes-data-completa').innerHTML = new Date().toDateString();
 
-//Criando variavel para gerar lista numerica dos dias
-const diasDoMes = document.querySelector('.dias');
-let dias = "";
-
-//criando variavel para identificar o ultimo dia de cada mes
-const ultimoDia = new Date(data.getFullYear(), data.getMonth() + 1,0).getDate();
+for(let x = primeiroDiaIndex; x > 0; x--){
+    dias += `<div class = "outro-mes">${diasMesAnterior - x + 1}</div>`;
+}
 
 //Metodo para gerar lista de dias do mes utilizando variavel do ultimoDia
 for(let i = 1; i <= ultimoDia; i++) {
-    // x += 10  x= x + 10
-    dias+= `<div>${i}</div>`;
-    diasDoMes.innerHTML = dias;
+    if(i === new Date().getDate() && data.getMonth() === new Date().getMonth()) {
+        dias += `<div>${i}</div>`;
+    } else {
+        dias += `<div>${i}</div>`;
+    }    
 }
 
+for(let j = 1; j <= proximosDias; j++) {
+    dias += `<div class = "outro-mes">${j}</div>`;
+    diasDoMes.innerHTML = dias;
+}
+}
+
+
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    data.setMonth(data.getMonth() - 1);
+    renderizarCalendario();
+})
+
+document.querySelector('.proximo').addEventListener('click', () => {
+    data.setMonth(data.getMonth() + 1);
+    renderizarCalendario();
+})
+
+renderizarCalendario();
